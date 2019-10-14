f891f51858e1e2bc8a8bb65dced8155346d9dcb9
MalardD <35556894+MalardD@users.noreply.github.com>
2019-10-14 14:17:19 +0200

    Add files via upload

---
+++ dist/precache-manifest.532981ebce7ac59bc93e20e56db529e9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "be0b56f20d4d2e68374a",
+    "url": "/js/chunk-vendors.f0419988.js"
+  },
+  {
+    "revision": "4d2bc3bdb155b3c7b684",
+    "url": "/js/app.27a73f0c.js"
+  },
+  {
+    "revision": "1fa7de38d69adc32b8d9",
+    "url": "/js/about.31eb3d68.js"
+  },
+  {
+    "revision": "6156c1e6a57730930c3bd888930a2c16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4d2bc3bdb155b3c7b684",
+    "url": "/css/app.d4670ae2.css"
+  }
+];
